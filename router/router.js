63e3f94d224ff31d385cf2f77dc8f554f56f5f03
HugoLaurent <lauhugopro@gmail.com>
2023-12-05 22:02:29 +0100

    Delete create_db.sql and router.js files

--- router/router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const logementController = require("../controller/logementController");
-
-const router = express.Router();
-
-router.get("/", logementController.getAllLogement);
