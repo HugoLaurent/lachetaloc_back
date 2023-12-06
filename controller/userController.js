0567dc29121693aca9e673153a0db621c4730ec5
HugoLaurent <lauhugopro@gmail.com>
2023-12-06 21:48:49 +0100

    Add createAnUser method to userController

--- controller/userController.js
+++ controller/userController.js
@@ -19,6 +19,47 @@ const userController = {
       res.status(500).json(error);
     }
   },
+  createAnUser: async (req, res) => {
+    const user = req.body;
+    if (user.pseudo === "") {
+      res.status(422).json({
+        code: 422,
+        message: "The pseudo is empty",
+      });
+      return;
+    }
+
+    const regexEmail = /^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+    if (!regexEmail.test(user.email)) {
+      res.status(422).json({
+        code: 422,
+        message: "The email is not valid",
+      });
+      return;
+    }
+
+    const regexPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{12,}$/;
+    if (!regexPassword.test(user.password)) {
+      res.status(422).json({
+        code: 422,
+        message:
+          "Password must contain at least 12 characters, one uppercase letter, one lowercase letter and one number",
+      });
+      return;
+    }
+
+    try {
+      const response = await User.create({
+        pseudo: req.body.pseudo,
+        email: req.body.email,
+        password: req.body.password,
+      });
+      res.json(response);
+    } catch (error) {
+      console.trace(error);
+      res.status(500).json(error);
+    }
+  },
 };
 
 module.exports = userController;
