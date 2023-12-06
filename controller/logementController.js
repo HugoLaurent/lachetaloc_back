806bd6b5a789c9cf6bd41643b96b14ba9377cbf8
HugoLaurent <lauhugopro@gmail.com>
2023-12-06 19:34:32 +0100

    Add new routes to get logement by user, room, and localisation

--- controller/logementController.js
+++ controller/logementController.js
@@ -19,6 +19,65 @@ const logementController = {
       res.status(500).json(error);
     }
   },
+  getLogementByUser: async (req, res) => {
+    try {
+      const response = await Logement.findAll({
+        where: {
+          utilisateur_id: req.params.utilisateur,
+        },
+      });
+      if (response.length === 0 || typeof response === "undefined") {
+        res
+          .status(404)
+          .json("Aucun logement occupé par cet utilisateur n'a été trouvé");
+      } else {
+        res.json(response);
+      }
+    } catch (error) {
+      console.trace(error);
+      res.status(500).json(error);
+    }
+  },
+  getLogementByRoom: async (req, res) => {
+    try {
+      const response = await Logement.findAll({
+        where: {
+          piece_id: req.params.piece,
+        },
+      });
+      if (response.length === 0 || typeof response === "undefined") {
+        res
+          .status(404)
+          .json(
+            "Aucun logement avec le nombre de pièces demandé n'a été trouvé"
+          );
+      } else {
+        res.json(response);
+      }
+    } catch (error) {
+      console.trace(error);
+      res.status(500).json(error);
+    }
+  },
+  getLogementByLocalisation: async (req, res) => {
+    try {
+      const response = await Logement.findAll({
+        where: {
+          localisation_id: req.params.localisation,
+        },
+      });
+      if (response.length === 0 || typeof response === "undefined") {
+        res
+          .status(404)
+          .json("Aucun logement dans la région demandée n'a été trouvé");
+      } else {
+        res.json(response);
+      }
+    } catch (error) {
+      console.trace(error);
+      res.status(500).json(error);
+    }
+  },
 };
 
 module.exports = logementController;
--- router/logement.js
+++ router/logement.js
@@ -6,5 +6,11 @@ const router = express.Router();
 
 router.get("/", logementController.getAllLogement);
 router.get("/:id", logementController.getOneLogement);
+router.get("/piece/:piece", logementController.getLogementByRoom);
+router.get(
+  "/localisation/:localisation",
+  logementController.getLogementByLocalisation
+);
+router.get("/utilisateur/:utilisateur", logementController.getLogementByUser);
 
 module.exports = router;
