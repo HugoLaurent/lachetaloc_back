857739c93c85581deba7b18108b63db2bd92ac4f
HugoLaurent <lauhugopro@gmail.com>
2023-12-07 19:04:40 +0100

    Update refresh token expiration

--- hook/auth/refreshToken.js
+++ hook/auth/refreshToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 function generateRefreshToken(user) {
-  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "1y" });
+  return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: "7d" });
 }
 
 const accessToken = generateAccessToken(user);
