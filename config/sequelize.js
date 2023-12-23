8e057945e9c15bd6ab0130e2e8302a2e5c9f63a5
HugoLaurent <lauhugopro@gmail.com>
2023-12-23 20:05:43 +0100

    Fix host env

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,12 +1,13 @@
 const Sequelize = require("sequelize");
+const hostDb = process.env.DB_HOST;
 
 const sequelize = new Sequelize(
   process.env.DB_DATABASE,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
-  process.env.DB_HOST,
+
   {
-    host: DB_HOST,
+    host: hostDb,
     dialect: "postgres",
     port: 5432,
   }
